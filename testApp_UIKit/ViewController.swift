96ae334a4f1255c5a6884920803a78e3f7b463a0
Joker4fun <Joker4fun1@gmail.com>
2023-11-02 23:02:05 +0300

    Initial Commit

---
+++ testApp_UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testApp_UIKit
+//
+//  Created by Антон Казеннов on 02.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
